52f5ceb08c625834b777f3b5a27188f27b8b0263
nagan7 <keenannagan8@gmail.com>
2025-05-15 14:23:01 +0200

    safety_working

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@
 //create an instance of bluetooth seial
 BluetoothSerial SerialBT;
 
-#define ignition 4
+#define ignition 13
 #define starter 2
-
+#define safety 23
 
 
 void setup() {
@@ -22,6 +22,9 @@ Serial.println("The device started, now you can pair it with bluetooth!");
 
 pinMode(ignition,OUTPUT);
 pinMode(starter,OUTPUT);
+pinMode(safety, OUTPUT);
+
+digitalWrite(safety,LOW);
 
 }
 
@@ -30,12 +33,22 @@ void loop() {
   if (SerialBT.available()) {
     char data=SerialBT.read();
     
-
+    
     if(data>=32 && data<=126){
       Serial.write(data); // this is for the serial monitor 
     }
+  
+
 
     switch(data){
+      case 'X':
+        digitalWrite(safety,HIGH);
+        break;
+
+      case 'x':
+        digitalWrite(safety,LOW);
+        break;        
+
       case 'W':
         digitalWrite(ignition,HIGH);
         break;
@@ -45,31 +58,39 @@ void loop() {
         break;
  
       case 'F':
-        digitalWrite(starter,HIGH);
-        delay(500);
-        digitalWrite(starter,LOW);
-        delay(1000);
+        if(digitalRead(safety)==LOW){
+          digitalWrite(starter,HIGH);
+          delay(500);
+          digitalWrite(starter,LOW);
+          delay(1000);
+        }
         break;
 
         case 'B':
+        if(digitalRead(safety)==LOW){
         digitalWrite(starter,HIGH);
         delay(700);
         digitalWrite(starter,LOW);
         delay(1000);
+        }
         break;
 
         case 'L':
+        if(digitalRead(safety)==LOW){
         digitalWrite(starter,HIGH);
         delay(900);
         digitalWrite(starter,LOW);
         delay(1000);
+        }
         break;
 
         case 'R':
+        if(digitalRead(safety)==LOW){
         digitalWrite(starter,HIGH);
         delay(1100);
         digitalWrite(starter,LOW);
         delay(1000);
+        }
         break;
     }
   }
